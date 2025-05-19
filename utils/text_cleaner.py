f08f9fa95a69970bfe83690fe34b0bf1b4bd09fa
al-ghalib <abdullah.ice.ru@gmail.com>
2025-05-20 02:01:59 +0600

    Added a text cleaner file which removes stopwords

---
+++ utils/text_cleaner.py
@@ -0,0 +1,15 @@
+import nltk
+import string
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+nltk.download("punkt")
+nltk.download("stopwords")
+
+STOPWORDS = set(stopwords.words("english"))
+
+
+def clean_text(text):
+    text = text.lower().translate(str.maketrans("", "", string.punctuation))
+    tokens = word_tokenize(text)
+    return [word for word in tokens if word.isalpha() and word not in STOPWORDS]
